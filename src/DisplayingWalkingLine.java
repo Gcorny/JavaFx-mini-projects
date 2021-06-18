b1e99c16b6e4a6ae52c39091290b7ce8d11a6b9f
Vinh Ngo <phucvinh1131@gmail.com>
2021-06-17 23:39:36 -0700

    successfully implemented the self-avoid walking feature of the line, and also added a few button for additional control

--- src/DisplayingWalkingLine.java
+++ src/DisplayingWalkingLine.java
@@ -1,7 +1,11 @@
 /*Displaying self-avoid random walking line*/
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class DisplayingWalkingLine extends Application{
@@ -14,8 +18,26 @@ public class DisplayingWalkingLine extends Application{
 	public void start(Stage stage) {
 		WalkingLine w = new WalkingLine();
 		
+		Button step = new Button("step");
+		Button pause = new Button("pause");
+		Button refresh = new Button("refresh");
+		Button start = new Button("start");
+		Button chillMode = new Button("chill");
+		HBox box = new HBox(refresh, start,pause, step, chillMode);
+		box.setAlignment(Pos.CENTER);
+		box.setSpacing(10);
+		BorderPane pane =new BorderPane();
+		pane.setCenter(w);
+		pane.setBottom(box);
+		start.setOnAction(e -> w.play());
+		refresh.setOnAction(e -> w.refresh());
+		pause.setOnAction(e -> w.pause());
+		step.setOnAction(e -> w.step((int)(Math.random()*4)));
+		chillMode.setOnAction(e -> w.chillMode());
 		
-		Scene scene  = new Scene(w, 500, 500);
+		
+		
+		Scene scene  = new Scene(pane, 500, 500);
 		stage.setScene(scene);
 		stage.show();
 	}
--- src/WalkingLine.java
+++ src/WalkingLine.java
@@ -6,7 +6,11 @@
  * functions that will draw new line depending on the random generated direction
  * a function that will check whether the line is at the edge.
  */
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.layout.Pane;
@@ -15,43 +19,85 @@ import javafx.util.Duration;
 
 
 public class WalkingLine extends Pane{
-	private List<Integer>[] graph; //this will help checking if the next position has already connected to the current
-	private double x = 250;
-	private double y = 250;
+	private Set<Pair> set = new HashSet<>(); //this will help checking if the next position has already connected to the current
+	private int x = 250;
+	private int y = 250;
 	private Timeline animation;
 	
 	
 	/** empty constructor **/
 	public WalkingLine() {
-		graph = new List[51];
+		set.add(new Pair(x,y));
 		animation = new Timeline(new KeyFrame(Duration.millis(50), e -> walk((int)(Math.random() * 4))));
 		animation.setCycleCount(Timeline.INDEFINITE);
-		animation.play();
+		
+	}
+	public void chillMode() {
+		animation = new Timeline(new KeyFrame(Duration.millis(50), e -> chilWalk((int)(Math.random() *4))));
+		animation.setCycleCount(Timeline.INDEFINITE);
 	}
 	
 	
 	public void walkUp() {
-		getChildren().add(new Line(x,y, x, y -10));
-		y -= 10;
+		int nextY = y -10;
+		if(!set.contains(new Pair(x,nextY))) {
+			getChildren().add(new Line(x,y, x, nextY));
+			y = nextY;
+			set.add(new Pair(x, nextY));
+		}
+		
 		
 	}
 	
 	public void walkDown() {
-		getChildren().add(new Line(x,y, x, y +10));
-		y += 10;
+		int nextY = y +10;
+		if(!set.contains(new Pair(x,nextY))) {
+			getChildren().add(new Line(x,y, x, nextY));
+			y = nextY;
+			set.add(new Pair(x, nextY));
 		}
+	}
 
 		
 	
 	public void walkLeft() {
-		getChildren().add(new Line(x, y, x - 10, y));
-		x -= 10;
+		int nextX = x -10;
+		if(!set.contains(new Pair(nextX, y))) {
+			getChildren().add(new Line(x, y, nextX, y));
+			x = nextX;
+			set.add(new Pair(nextX, y));
+		}
+		
 	}
 	
 	public void walkRight() {
-		getChildren().add(new Line(x, y , x + 10, y));
-		x += 10;
+		int nextX = x +10;
+		if(!set.contains(new Pair(nextX, y))) {
+			getChildren().add(new Line(x, y, nextX, y));
+			x = nextX;
+			set.add(new Pair(nextX, y));
+		}
 		
+	}
+	public void up() {
+		getChildren().add(new Line(x, y, x , y -10));
+		y -= 10;
+	}
+	
+	public void down() {
+		getChildren().add(new Line(x ,y, x , y +10));
+		y += 10;
+		
+	}
+	
+	public void left() {
+		getChildren().add(new Line(x, y, x -10, y));
+		x -= 10;
+	}
+	
+	public void right() {
+		getChildren().add(new Line(x, y, x + 10, y));
+		x += 10;
 	}
 	
 	public void play() {
@@ -62,21 +108,56 @@ public class WalkingLine extends Pane{
 		animation.pause();
 	}
 	
+	public void refresh() {
+		getChildren().clear();
+		x = 250;
+		y = 250;
+		set.clear();
+		play();
+	}
+	
+	public void step(int direction) {
+		walk(direction);
+	}
+	
 
 	
 	public boolean isEdge() {
 		if(x == 0 || x == getWidth() || y == 0 || y == getHeight()) return true;
 		return false;
 	}
+	public boolean isCornered() {
+		if(set.contains(new Pair(x + 10, y)) && set.contains(new Pair(x - 10, y)) &&
+				set.contains(new Pair(x, y + 10)) && set.contains(new Pair(x , y -10))) return true;
+		return false;
+	}
 	
-	
+	public void chilWalk(int direction) {
+		if(isEdge()) { //stop once the line reaches the edge.
+			animation.stop();
+			
+		}
+		if(isCornered()) {
+			animation.stop();
+		}
+		
+		switch(direction) {
+		case 0: up(); break;
+		case 1: down(); break;
+		case 2: left(); break;
+		case 3: right(); break;
+		}
+		
+	}
 	public void walk(int direction) {
 		if(isEdge()) { //stop once the line reaches the edge.
-			getChildren().clear();
-			x = 250;
-			y = 250;
-			return;
+			animation.stop();
+			
 		}
+		if(isCornered()) {
+			animation.stop();
+		}
+		
 		switch(direction) {
 		case 0: walkUp(); break;
 		case 1: walkDown(); break;
@@ -85,7 +166,27 @@ public class WalkingLine extends Pane{
 		}
 		
 	}
-	
-	
-	
+	class Pair{
+		protected int x;
+		protected int y;
+		
+		public Pair(int x, int y) {
+			this.x = x;
+			this.y = y;
+			
+		}
+		public int hashCode() {
+			return x + y;
+		}
+		public int getX() {
+			return x;
+		}
+		
+		public int getY() {
+			return y;
+		}
+		public boolean equals(Object o ) {
+			return x == ((Pair)o).x && y ==((Pair)o).y;
+		}
+	}
 }
